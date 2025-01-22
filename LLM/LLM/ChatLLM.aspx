96f15d6e8c121bea474b41ee0186fe7734fe573a
Colter Swanson <cswans11@vols.utk.edu>
2025-01-22 14:06:22 -0500

    Added ASP.Net Front end for interaction with LLM

---
+++ LLM/LLM/ChatLLM.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ChatLLM.aspx.cs" Inherits="LLMWebApp.ChatLLM" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Chat LLM</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Chat with LLM</h1>
+            <asp:Label ID="lblPrompt" runat="server" Text="Enter your question:"></asp:Label><br />
+            <asp:TextBox ID="txtPrompt" runat="server" Width="400px"></asp:TextBox><br /><br />
+            <asp:Button ID="btnSubmit" runat="server" Text="Generate Response" OnClick="btnSubmit_Click" />
+            <br /><br />
+            <asp:Label ID="lblResponse" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ LLM/LLM/ChatLLM.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace LLMWebApp
+{
+    public partial class ChatLLM : System.Web.UI.Page
+    {
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string prompt = txtPrompt.Text;
+            string response = GetResponse(prompt);
+
+            lblResponse.Text = $"Response: {response}";
+        }
+
+        public string GetResponse(string question)
+        {
+            using (var client = new TcpClient("127.0.0.1", 65432))
+            using (var stream = client.GetStream())
+            {
+                byte[] data = Encoding.UTF8.GetBytes(question);
+                stream.Write(data, 0, data.Length);
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                return Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            }
+        }
+    }
+}
+
